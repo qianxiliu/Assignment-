e1f22395714ee1f8a4fa1cf8afbec6e8002301c2
Qianxi Liu <qianxiliu@usf.edu>
2021-01-27 13:12:57 -0500

    version2.0

--- Assignment 1/Program.cs
+++ Assignment 1/Program.cs
@@ -8,42 +8,52 @@ namespace Assignment1_Spring2021
     class Program
     {
 
-
-
-
-        //Question 1
-        private static void printTriangle(int n)//create a new method which invoke by the method PrintPattern
+        static void Main(string[] args)
         {
+            int n = 5;
+            PrintTriangle(n);
+        }
 
 
-            try
+            //Question 1
+            private static void PrintTriangle(int n)//create a new method which invoke by the method PrintPattern
             {
-                int i, j;
 
-                Console.WriteLine("Q1 : Enter the number of rows for the traingle:");
 
-                n = Convert.ToInt32(Console.ReadLine());
-
-                for (i = 0; i < n; i++)
+                try
                 {
-                    for (j = 1; j <= n - i; j++)
-                        Console.WriteLine(" ");
-                    for (j = 1; j <= 2 * i - 1; j++)
+                    int i, j,k;
+
+                    Console.WriteLine("Q1 : Enter the number of rows for the traingle:");
+
+                    n = Convert.ToInt32(Console.ReadLine());
+
+                    for (i = 1; i<=n; i++)
+                    {
+                        for (j = n; j >= i; j--)
+                        {
+                        Console.Write(" ");
+                        }
+
+                       for (k = 1; k <= (2*i-1); k++)
+                       {
                         Console.Write("*");
-                    
+                       }
+
+                    Console.WriteLine();
+                    }
+                
+
+                    PrintTriangle(n);
+                    Console.ReadLine();
 
                 }
-
-
-                printTriangle(n);
-                Console.WriteLine();
-
-            }
-            catch (Exception)
-            {
+                catch (Exception)
+                {
                 throw;
+                }
             }
-        }
+        
 
                 
 
