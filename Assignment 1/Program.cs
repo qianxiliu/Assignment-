0e34e3b4deaee5920cef029963aa47de1ccafb08
Qianxi Liu <qianxiliu@usf.edu>
2021-01-27 12:29:14 -0500

    2.0

--- Assignment 1/Program.cs
+++ Assignment 1/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Assignment_1
+
+namespace Assignment1_Spring2021
 {
     class Program
     {
-        static void Main(string[] args)
+
+
+
+
+        //Question 1
+        private static void printTriangle(int n)//create a new method which invoke by the method PrintPattern
         {
-            Console.WriteLine("Hello World!");
+
+
+            try
+            {
+                int i, j;
+
+                Console.WriteLine("Q1 : Enter the number of rows for the traingle:");
+
+                n = Convert.ToInt32(Console.ReadLine());
+
+                for (i = 0; i < n; i++)
+                {
+                    for (j = 1; j <= n - i; j++)
+                        Console.WriteLine(" ");
+                    for (j = 1; j <= 2 * i - 1; j++)
+                        Console.Write("*");
+                    
+
+                }
+
+
+                printTriangle(n);
+                Console.WriteLine();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
+
+                
+
+              
+            
+
+
+        
     }
 }
+
+
